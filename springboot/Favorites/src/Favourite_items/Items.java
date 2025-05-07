32b4f6e0c0e749e170123b106427f99087287a68
Gauri <gauriM304@gmail.com>
2025-05-07 09:09:36 +0530

    Core java programs

---
+++ springboot/Favorites/src/Favourite_items/Items.java
@@ -0,0 +1,11 @@
+package Favourite_items;
+
+public class Items {
+    public static void main(String[] args) {
+        System.out.println("Cats");
+        System.out.println("Pasta");
+        System.out.println("Songs");
+        System.out.println("Travelling");
+        System.out.println("Shopping");
+    }}
+
