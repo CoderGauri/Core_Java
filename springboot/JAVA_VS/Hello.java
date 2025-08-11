7516d7e5b8019c288a2353567ff858fb3700a117
Gauri <gauriM304@gmail.com>
2025-08-11 20:24:34 +0530

    Basics added

---
+++ springboot/JAVA_VS/Hello.java
@@ -0,0 +1,41 @@
+class Hello {
+    public static void main(String a[])
+
+    {
+        // Variables
+        int num1 = 3;
+        int num2 = 7;
+        int result = num1 + num2;
+
+        // System.out.println(num1);
+        // System.out.println(num2);
+        // System.out.println(result);
+        // System.out.println("Hello World");
+        // System.out.println(5 + 9);
+
+        // Data Types
+        int num = 1;
+        byte by = 127;
+        long lg = 54678;
+        short sh = 558;
+        char c = 'k';
+        boolean b = true;
+        float f = 5.9f;
+        double d = 5.8;
+        // System.out.println(num);
+        // System.out.println(by);
+        // System.out.println(lg);
+        // System.out.println(sh);
+        // System.out.println(c);
+        // System.out.println(b);
+        // System.out.println(f);
+        // System.out.println(d);
+
+    //Type casting and conversion
+    byte b1 = 127;
+    System.out.println(b1);
+    
+
+    }
+
+}
