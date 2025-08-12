09c95b3ffc16634147097179405ac1685c00b348
Gauri <gauriM304@gmail.com>
2025-08-12 20:25:32 +0530

    If else in Java

---
+++ springboot/JAVA_VS/Conditional.java
@@ -0,0 +1,12 @@
+public class Conditional {
+    public static void main (String a[]){
+        int time = 9;
+        if(time <= 10){
+             System.out.println("Good Morning");
+        }
+        else{
+            System.out.println("Good evening ");
+        }
+       
+    }
+}
