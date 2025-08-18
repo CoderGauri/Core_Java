568cd10c1eefccf9b555122992ba61cf60914c4d
Gauri <gauriM304@gmail.com>
2025-08-18 15:04:42 +0530

    spring boot first application

--- springboot/JAVA_VS/Conditional.java
+++ springboot/JAVA_VS/Conditional.java
@@ -1,5 +1,6 @@
 public class Conditional {
     public static void main (String a[]){
+        //example one
         int time = 9;
         if(time <= 10){
              System.out.println("Good Morning");
@@ -7,6 +8,13 @@ public class Conditional {
         else{
             System.out.println("Good evening ");
         }
-       
+       //example 2 
+       int age = 20;
+       if (age>=18 && age<=30){
+        System.out.println("Eligible to enter college");
+    }
+    else{
+        System.out.println("Not eligble to enter college");
+    }
     }
 }
