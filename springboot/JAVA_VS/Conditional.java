6dc534ff25eba4450a1e46a55d9b2c22de6ba85f
Gauri <gauriM304@gmail.com>
2025-08-19 20:00:20 +0530

    if else if example

--- springboot/JAVA_VS/Conditional.java
+++ springboot/JAVA_VS/Conditional.java
@@ -1,20 +1,36 @@
 public class Conditional {
-    public static void main (String a[]){
-        //example one
+    public static void main(String a[]) {
+        // example one
         int time = 9;
-        if(time <= 10){
-             System.out.println("Good Morning");
-        }
-        else{
+        if (time <= 10) {
+            System.out.println("Good Morning");
+        } else {
             System.out.println("Good evening ");
         }
-       //example 2 
-       int age = 20;
-       if (age>=18 && age<=30){
-        System.out.println("Eligible to enter college");
-    }
-    else{
-        System.out.println("Not eligble to enter college");
-    }
+        // example 2
+        int age = 20;
+        if (age >= 18 && age <= 30) {
+            System.out.println("Eligible to enter college");
+        } else {
+        }
+       //if else if
+       int marks = 72;
+
+if (marks >= 90) {
+    System.out.println("Grade A");
+} else if (marks >= 75) {
+    System.out.println("Grade B");
+} else if (marks >= 50) {
+    System.out.println("Grade C");
+} else {
+    System.out.println("Fail");
+}
+//trenary operator 
+
+int n = 5;
+String result = "even" ;
+result = n%2 == 0 ? "even" : "odd";
+System.out.println(result);
+
     }
 }
