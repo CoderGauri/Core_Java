eb9254bbaa1ad91e588f0696ca5ec1be9420b0fe
Gauri <gauriM304@gmail.com>
2025-09-15 09:14:10 +0530

    Methods in Java

--- springboot/JAVA_VS/Day1/Method.java
+++ springboot/JAVA_VS/Day1/Method.java
@@ -1,12 +1,23 @@
 package Day1;
 
 class Computer {
-    
+    public void playMusic(){
+        System.out.println("Music Playing");
+    }
+    public String getMeAPen(int cost){
+       if(cost >= 10)
+       return "pen";
+       else 
+       return "Nothing";
+    }
 }
 
 public class Method {
 
     public static void main(String[] args) {
-        
+        Computer obj = new Computer();
+        obj.playMusic();
+        String str = obj.getMeAPen(10);
+        System.out.println(str);
     }
 }
